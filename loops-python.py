dd57879a5fa9bf170f84d98162675dbbd5faa788
Mcegonha. <77519446+messias713@users.noreply.github.com>
2024-07-25 15:35:36 +0100

    Add files via upload

---
+++ loops-python.py
@@ -0,0 +1,14 @@
+palavras = ["amor", "paz", "fé", "esperança", "gratidão", "compaixão", "harmonia", "iluminação", "sabedoria", "bondade"]
+
+while True:
+    for palavra in palavras:
+        print(palavra)
+        <----------------------novo codigo->
+   import time
+
+palavras = ["amor", "paz", "fé", "esperança", "gratidão", "compaixão", "harmonia", "iluminação", "sabedoria", "bondade"]
+
+while True:
+    for palavra in palavras:
+        print(palavra)
+        time.sleep(1)
